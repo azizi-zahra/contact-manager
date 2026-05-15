8e4a224e23485235b77f475863cb379c6d9fcac6
zahra azizi <azizi.zahra.tehran@gmail.com>
2026-05-15 15:47:28 +0330

    feat: add React frontend with Vite

--- models/__init__.py
+++
@@ -1,3 +0,0 @@
-from models.user import User, UserRegister, UserLogin, UserResponse
-from models.contact import Contact, ContactCreate, ContactUpdate, ContactResponse
-from models.label import Label, LabelCreate, LabelUpdate, LabelResponse, ContactLabel
--- routers/__init__.py
+++
@@ -1,3 +0,0 @@
-from routers.contact import router as contact_router
-from routers.auth import router as auth_router
-from routers.label import router as label_router
